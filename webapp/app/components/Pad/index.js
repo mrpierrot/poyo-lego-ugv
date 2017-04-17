44bc46d09f9117c0ea168b6f62f26fd489c9b078
Pierre Chabiland <pierrot@poyo.home>
2017-04-17 15:34:21 +0200

    pad ( wip )

--- webapp/app/components/Pad/index.js
+++ webapp/app/components/Pad/index.js
@@ -3,29 +3,40 @@ import { html } from 'snabbdom-jsx';
 
 function intent(DOM) {
     const pad$ = DOM.select('.pad-base');
-    //pad$.events('mousedown').mapTo(true);
-    return pad$.events('mousemove').map((e) => ({x: e.layerX,y: e.layerY}))
+    const root$ = DOM.select(':root');
+    const mouseDown$ = pad$.events('mousedown').mapTo(true);
+    const mouseUp$ = root$.events('mouseup').mapTo(true).debug();
+    //const mouseLeave$ = root$.events('mouseleave').mapTo(true).debug();
+    const mouseMove$ = root$.events('mousemove').debug();
+
+    return mouseDown$
+        .map(mouseDown => mouseMove$
+            .map(e => ({ x: e.offsetX, y: e.offsetY }))
+            .debug()
+            .endWhen(mouseUp$)
+        )
+        .flatten();
 }
 
 function model(action$, props$) {
     return props$
         .map((props) => action$
-            .map(({x,y}) => ({ x, y }))
+            .map(({ x, y }) => ({ x, y }))
             .startWith(props)
         )
         .flatten();
 }
 
 function view(state$) {
-    return (state$.map(({x,y}) =>
+    return (state$.map(({ x, y }) =>
         <div className="pad-base">
-            <div className="pad-button" style={{left:`${x}px`,top:`${y}px`}}></div>
+            <div className="pad-button" style={{ left: `${x}px`, top: `${y}px` }}></div>
         </div>
     ))
 }
 
 
-function _Pad({ DOM, props = xs.of({x:0,y:0}) }) {
+function _Pad({ DOM, props = xs.of({ x: 0, y: 0 }) }) {
 
     const action$ = intent(DOM);
     const state$ = model(action$, props);
