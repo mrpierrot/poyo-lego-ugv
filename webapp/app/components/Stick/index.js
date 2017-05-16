429548aeb93501c6c62240f70445c32318998e0f
Pierre Chabiland <pierrot@poyo.local>
2017-05-16 12:44:05 +0200

    improve stick back

--- webapp/app/components/Stick/index.js
+++ webapp/app/components/Stick/index.js
@@ -3,11 +3,13 @@ import tween from 'xstream/extra/tween';
 import { html } from 'snabbdom-jsx';
 import _ from 'lodash';
 
-function borderRate(val) {
-    if (val < -1) return -1;
-    if (val > 1) return 1;
+
+function capValue(val,min,max) {
+    if (val < min) return min;
+    if (val > max) return max;
     return val;
 }
+
 
 function modeToClasses(mode) {
     switch (mode) {
@@ -19,7 +21,7 @@ function modeToClasses(mode) {
 
 function getTargetTouch(targetTouches, e) {
     const result = _.find(e.changedTouches, (o) => _.some(targetTouches, a => a.identifier == o.identifier));
-    return result?result:false;
+    return result ? result : false;
 }
 
 function isSameTouchAction(targetTouches, e) {
@@ -27,24 +29,25 @@ function isSameTouchAction(targetTouches, e) {
 }
 
 function intent(DOM) {
-    const stick$ = DOM.select('.stick-base');
-    const root$ = DOM.select('body');
-    const touchStart$ = stick$.events('touchstart');
-    const touchEnd$ = root$.events('touchend');
-    const touchCancel$ = root$.events('touchcancel');
-    const touchMove$ = root$.events('touchmove');
+    const stick = DOM.select('.stick-base');
+    const root = DOM.select('body');
+    const touchStart$ = stick.events('touchstart');
+    const touchEnd$ = root.events('touchend');
+    const touchCancel$ = root.events('touchcancel');
+    const touchMove$ = root.events('touchmove');
 
     return touchStart$
         .map(touchStartEvent => {
             const { top, left, width, height } = touchStartEvent.currentTarget.getBoundingClientRect();
             const targetTouches = touchStartEvent.targetTouches;
             const endAction$ = xs.merge(touchEnd$, touchCancel$)
-                                .map(e => getTargetTouch(targetTouches, e))
-                                .filter(touch => touch);
+                .map(e => getTargetTouch(targetTouches, e))
+                .filter(touch => touch);
             const moveAction$ = touchMove$
                 .filter(e => isSameTouchAction(targetTouches, e))
                 .map(e => {
                     return {
+                        type: 'manual',
                         x: e.targetTouches[0].clientX,
                         y: e.targetTouches[0].clientY,
                         top, left, width, height
@@ -53,28 +56,14 @@ function intent(DOM) {
                 .endWhen(endAction$);
 
             const repositionneAction$ = endAction$.take(1).map((touch) => {
-                const duration = 500;
-                const tweenX = tween({
-                    from: touch.clientX,
-                    to: left + width * 0.5,
-                    ease: tween.power2.easeInOut,
-                    duration
-                });
-
-                const tweenY = tween({
-                    from: touch.clientY,
-                    to: top + height * 0.5,
-                    ease: tween.power2.easeInOut,
-                    duration
-                })
-
-                return xs.combine(tweenX, tweenY)
-                    .map(([x, y]) => ({
-                        x, y, top, left, width, height
-                    }));
+                return {
+                    type: 'auto',
+                    touchX: touch.clientX,
+                    touchY: touch.clientY,
+                    top, left, width, height
+                }
             })
-            .flatten();
-           
+
             return xs.merge(
                 moveAction$,
                 repositionneAction$
@@ -86,21 +75,46 @@ function intent(DOM) {
 function model(action$, props$) {
     return props$
         .map((props) => action$
-            .map(({ x,y,top, left, width, height }) => {
+            .map((data) => {
+                if (data.type != 'auto') return xs.of(data);
                 const padding = props.padding;
-                const w = width - padding*2;
-                const h = height - padding*2;
-                const rateX = (x - left - w*0.5 - padding )/(w*0.5);
-                const rateY = (y - top - h*0.5 - padding)/(h*0.5);
-                switch(props.mode){
+                const { touchX, touchY, top, left, height, width } = data;
+                const fromX = capValue(touchX,left-padding,left + width - padding);
+                const fromY = capValue(touchY,top-padding,top + height - padding);
+                const tweenX = tween({
+                    from: fromX,
+                    to: left + width * 0.5,
+                    ease: props.backDurationEase,
+                    duration: props.backDuration
+                });
+
+                const tweenY = tween({
+                    from: fromY,
+                    to: top + height * 0.5,
+                    ease: props.backDurationEase,
+                    duration: props.backDuration
+                })
+
+                return xs.combine(tweenX, tweenY)
+                    .map(([x, y]) => ({
+                        x, y, top, left, width, height
+                    }));
+            }).flatten()
+            .map(({ x, y, top, left, width, height }) => {
+                const padding = props.padding;
+                const w = width - padding * 2;
+                const h = height - padding * 2;
+                const rateX = (x - left - w * 0.5 - padding) / (w * 0.5);
+                const rateY = (y - top - h * 0.5 - padding) / (h * 0.5);
+                switch (props.mode) {
                     default:
-                    case ALL_DIR_STICK_MODE: return { rateX:borderRate(rateX), rateY:borderRate(rateY), mode:props.mode, padding }
-                    case VERTICAL_STICK_MODE: return { rateX:0, rateY:borderRate(rateY), mode:props.mode, padding }
-                    case HORIZONTAL_STICK_MODE: return { rateX:borderRate(rateX), rateY:0, mode:props.mode, padding }
+                    case ALL_DIR_STICK_MODE: return { rateX: capValue(rateX,-1,1), rateY: capValue(rateY,-1,1), mode: props.mode, padding }
+                    case VERTICAL_STICK_MODE: return { rateX: 0, rateY: capValue(rateY,-1,1), mode: props.mode, padding }
+                    case HORIZONTAL_STICK_MODE: return { rateX: capValue(rateX,-1,1), rateY: 0, mode: props.mode, padding }
                 }
-                
+
             })
-            .startWith(props)
+            .startWith({ ...props, rateX: 0, rateY: 0 })
         )
         .flatten()
         .remember();
@@ -108,7 +122,7 @@ function model(action$, props$) {
 
 function view(state$) {
     return (state$.map(({ rateX, rateY, mode, padding }) =>
-        <div className={"stick-base "+modeToClasses(mode)}>
+        <div className={"stick-base " + modeToClasses(mode)}>
             <div className="stick-decorator-1"></div>
             <div className="stick-decorator-2"></div>
             <div className="stick-button" style={{ left: `calc(50% + ${rateX} * (50% - ${padding}px))`, top: `calc(50% + ${rateY} * (50% - ${padding}px))` }}></div>
@@ -116,9 +130,9 @@ function view(state$) {
     ))
 }
 
-export function Stick({ DOM, props$ = xs.of({})}) {
-    const defaultProps$ = xs.of({ rateX: 0, rateY: 0, mode: ALL_DIR_STICK_MODE,padding: 16 });
-    const newProps$ = xs.combine( defaultProps$, props$ ).map(([a,b]) => ({...a,...b}));
+export function Stick({ DOM, props$ = xs.of({}) }) {
+    const defaultProps$ = xs.of({ mode: ALL_DIR_STICK_MODE, padding: 16, backDuration: 500, backEase: tween.power2.easeInOut });
+    const newProps$ = xs.combine(defaultProps$, props$).map(([a, b]) => ({ ...a, ...b }));
     const action$ = intent(DOM);
     const state$ = model(action$, newProps$);
     const vdom$ = view(state$);
